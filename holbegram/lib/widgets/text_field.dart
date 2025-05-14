302574956177dc16f8d17362fd08cd355afdc959
ZidiEya <91125384+ZidiEya@users.noreply.github.com>
2025-05-14 20:35:26 +0100

    Update text_field.dart

--- holbegram/lib/widgets/text_field.dart
+++ holbegram/lib/widgets/text_field.dart
@@ -1,17 +1,3 @@
-import 'package:flutter/material.dart';
-
-class CustomTextField extends StatelessWidget {
-  final TextEditingController controller;
-  final String hintText;
-  final bool obscureText;
-
-  const CustomTextField({
-    super.key,
-    required this.controller,
-    required this.hintText,
-    this.obscureText = false,
-  });
-
   @override
   Widget build(BuildContext context) {
     return TextField(
@@ -21,6 +7,53 @@ class CustomTextField extends StatelessWidget {
         hintText: hintText,
         border: const OutlineInputBorder(),
         contentPadding: const EdgeInsets.symmetric(horizontal: 16),
+      ),
+    );
+  }
+}
+import 'package:flutter/material.dart';
+
+class TextFieldInput extends StatelessWidget {
+  final TextEditingController controller;
+  final bool isPassword;
+  final String hintText;
+  final Widget? suffixIcon;
+  final TextInputType keyboardType;
+
+  const TextFieldInput({
+    super.key,
+    required this.controller,
+    required this.isPassword,
+    required this.hintText,
+    this.suffixIcon,
+    required this.keyboardType,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      keyboardType: keyboardType,
+      obscureText: isPassword,
+      cursorColor: const Color.fromARGB(218, 226, 37, 24),
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+        hintText: hintText,
+        suffixIcon: suffixIcon,
+        contentPadding: const EdgeInsets.all(8),
+        filled: true,
+        border: OutlineInputBorder(
+          borderSide: const BorderSide(color: Colors.transparent),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: Colors.transparent),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: Colors.transparent),
+          borderRadius: BorderRadius.circular(8),
+        ),
       ),
     );
   }
